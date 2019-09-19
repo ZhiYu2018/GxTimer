4532b663c8cd6058ad37e33fc205ce592976f6f3
Simon.Chen <forbelief@126.com>
2019-09-19 13:02:12 +0800

    增加表结构

--- README.md
+++ README.md
@@ -2,6 +2,56 @@
 1、定时回调系统 
 ## 设计意图
 * 尝试型、定时调用第三方接口。
-* 实时、保障型回调第三方接口。
+* 实时、保障型通知第三方。
 ## 使用方法
 
+* 提交定时任务，通过调用 http://url/submit/job
+* 提交实时保障型通知 http://url/submit/notify
+* 终态(成功，最终失败) 回调给使用方 http://url/callback，URL 是使用方的。
+
+## 请求内容
+
+* 定时补偿任务、实时通知的请求格式是application/json
+
+```java
+class GxTimerRequest{
+    String appId;
+    String jobId;
+    int reqType;
+    int dataType;
+    long timeUsed; /**需要尝试的时长，单位秒，0 采用系统默认的时长**/
+    String reqUrl;
+    String cbUrl;
+    String reqBody;
+    String reqHeader;
+}
+```
+
+响应的内容格式，也是application/json
+
+```java
+class GxTimerResponse{
+    String appId;
+    String jobId;
+    int    status; /**200 成功，其他的失败**/
+    String msg;    /**失败原因**/
+}
+```
+
+* 回调给调用方的请求也是application/json
+
+```java
+class GxTimeCbRequest{
+    String appId;
+    String jobId;
+    int    status; /**200 成功，其他的失败**/
+    String msg;    /**失败原因**/
+    String body;   /**第三方返回的内容**/
+}
+```
+
+## 关键设计
+
+* 可靠尝试 采用指数退避算法，避免请求风暴。
+* 采用异步方式，单节点执行，双节点冗余。
+* 利用时间增量，进行任务轮询，定时任务执行时间间隔是1秒。
---
+++ timer.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE gx_timer
+
+CREATE TABLE `timer_job`(
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `app_id` varchar(32) NOT NULL DEFAULT '' COMMENT '接入方ID',
+  `job_id` varchar(64) NOT NULL DEFAULT '' COMMENT '目标事件ID',
+  `job_status` int NOT NULL DEFAULT 0 COMMENT '0 Idle, 1 成功, -1 失败, 2 在尝试中',
+  `req_type`   int  NOT NULL DEFAULT 0 COMMENT '0 GET, 1 POST, 2 PUT',
+  `data_type`  int NOT NULL DEFAULT 0 COMMENT '数据类型, 1 text/xml, 2 application/json, 3 application/x-www-form-urlencoded',
+  `req_url` varchar(512) NOT NULL DEFAULT '' COMMENT '被调用的URL',
+  `cb_url`  varchar(512) NOT NULL DEFAULT '' COMMENT '回调给调用方的URL，在成功或者最终失败调用',
+  `req_body` varchar(8192) NOT NULL DEFAULT '' COMMENT '请求内容',
+  `req_header` varchar(1024) NOT NULL DEFAULT '' COMMENT '请求头部',
+  `next_time` long NOT NULL DEFAULT 0 COMMENT '下次尝试时间',
+  `end_time`  long NOT NULL DEFAULT 0 COMMENT '结束时间',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_job` (`app_id`, `job_id`), 
+  KEY `idx_nt` (`next_time`)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8
+
+
+CREATE TABLE `timer_cb`(
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `app_id` varchar(32) NOT NULL DEFAULT '' COMMENT '接入方ID',
+  `job_id` varchar(64) NOT NULL DEFAULT '' COMMENT '目标事件ID',
+  `job_status` int NOT NULL DEFAULT 0 COMMENT '0 Idle, 1 成功, -1 失败, 2 在尝试中',
+  `req_type`   int  NOT NULL DEFAULT 0 COMMENT '0 GET, 1 POST, 2 PUT',
+  `data_type`  int NOT NULL DEFAULT 0 COMMENT '数据类型, 1 text/xml, 2 application/json, 3 application/x-www-form-urlencoded',
+  `req_url` varchar(512) NOT NULL DEFAULT '' COMMENT '被调用的URL',
+  `cb_url`  varchar(512) NOT NULL DEFAULT '' COMMENT '回调给调用方的URL，在成功或者最终失败调用',
+  `req_body` varchar(8192) NOT NULL DEFAULT '' COMMENT '请求内容',
+  `req_header` varchar(1024) NOT NULL DEFAULT '' COMMENT '请求头部',
+  `next_time` long NOT NULL DEFAULT 0 COMMENT '下次尝试时间',
+  `end_time`  long NOT NULL DEFAULT 0 COMMENT '结束时间',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_job` (`app_id`, `job_id`), 
+  KEY `idx_nt` (`next_time`)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8
